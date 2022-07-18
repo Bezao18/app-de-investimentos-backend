a54614ccad91b55b4e4f27bd1f099b19672a9def
Bernardo <bernardo.r.s18@gmail.com>
2022-07-17 23:07:30 -0300

    feat(ativos.controller + service): implementa função que busca ativo por id

--- src/controllers/ativos.controller.ts
+++ src/controllers/ativos.controller.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from "express";
-import { /* getByAsset, getAll, */ getClientPortfolio } from '../services/ativos.service';
+import { getAsset, getClientPortfolio } from '../services/ativos.service';
 
 const getByParameter = async (req: Request, res: Response) => {
   const { cliente, ativo } = req.query;
   if (cliente) {
     const clientPortfolio = await getClientPortfolio(Number(cliente));
     return res.status(200).json(clientPortfolio)
-  } /* if (ativo) {
-    const object = await getByAsset(ativo).;
-    return res.status(418).json('PESQUISA POR ATIVO')
-  }     const object = await getAll();
+  } if (ativo) {
+    const asset = await getAsset(Number(ativo));
+    return res.status(418).json(asset)
+  }     /* const object = await getAll();
   return res.status(418).json('PESQUISA GERAL') */
 }
 
--- src/services/ativos.service.ts
+++ src/services/ativos.service.ts
@@ -1,15 +1,16 @@
 import sequelize from 'sequelize';
 import IClient from '../interfaces/IClient'
+const { Ativo } = require('../database/models');
 import IOrder from '../interfaces/IOrder';
 import getClientsOrders from '../utils/getClientsOrders';
 
 const getClientPortfolio = async (clientId: number): Promise<any> => {
   const buyOrders: IOrder[] = await getClientsOrders(clientId, 'Compra');
-  const sellOrders: IOrder[] = await  getClientsOrders(clientId, 'Venda');
+  const sellOrders: IOrder[] = await getClientsOrders(clientId, 'Venda');
 
   const clientPortfolio = buyOrders.map((compra: any, i: number) => {
     const venda = sellOrders[i]
-    const { CodCliente, CodAtivo, Ativo: {Valor} } = compra
+    const { CodCliente, CodAtivo, Ativo: { Valor } } = compra
     if (venda) {
       const QtdeAtivo = compra.QtdeAtivo - venda.QtdeAtivo
       const object = {
@@ -18,14 +19,14 @@ const getClientPortfolio = async (clientId: number): Promise<any> => {
         QtdeAtivo,
         Valor
       }
-      return  object
+      return object
     }
     const object = {
       CodCliente,
       CodAtivo,
       QtdeAtivo: compra.QtdeAtivo,
       Valor
-    } 
+    }
     return object
   }
   )
@@ -37,12 +38,13 @@ const getClientPortfolio = async (clientId: number): Promise<any> => {
 
 
 
-// const getByAsset = async (assetId: number) => {
+const getAsset = async (assetId: number) => {
+  const asset = await Ativo.findByPk(assetId)
+  return asset;
+}
 
-// }
+const getAll = async () => {
 
-// const getAll = async () => {
+}
 
-// }
-
-export { /* getAll, getByAsset, */ getClientPortfolio }
+export { getAll, getAsset, getClientPortfolio }
