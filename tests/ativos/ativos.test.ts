2697b65e9f4139f58f837f3f2318d3e90d9e3d54
Bernardo <bernardo.r.s18@gmail.com>
2022-07-22 18:52:23 -0300

    test(ativos.test): testa rota /ativos/:CodCliente

--- tests/ativos/ativos.test.ts
+++ tests/ativos/ativos.test.ts
@@ -3,6 +3,7 @@ import chaiHttp from 'chai-http';
 import { before } from 'mocha';
 import app from '../../src/app';
 import { IAtivo } from '../../src/interfaces';
+import HTTPErrorMessage from '../../src/utils/HTTPErrorMessage';
 
 
 chai.use(chaiHttp);
@@ -16,13 +17,63 @@ describe('Testa a rota /ativos', () => {
     status = response.status;
     body = response.body;
   })
-  it('A requisição GET para a rota retorna o status 200', async () => {
+  it('A requisição GET para a rota retorna o status 200', () => {
     expect(status).to.be.equal(200);
   });
-  it('A requisição GET para a rota retorna um array de objetos', async () => {
-    expect(body).to.be.an('array');    
+  it('A requisição GET para a rota retorna um array de objetos', () => {
+    expect(body).to.be.an('array');
   });
-  it('Os objetos retornados no array possuem as chaves "CodAtivo", "QtdeAtivo" e "Valor"', async () => {
+  it('Os objetos retornados no array possuem as chaves "CodAtivo", "QtdeAtivo" e "Valor"', () => {
     expect(body[0]).to.include.all.keys("CodAtivo", "QtdeAtivo", "Valor");
   });
+})
+
+describe('Testa a rota /ativos/:CodAtivo', () => {
+  let body: IAtivo;
+  let status: number;
+  describe('Quando é passado o CodAtivo de um ativo existente', () => {
+    before(async () => {
+      const response = await chai.request(app).get('/ativos/1')
+      status = response.status;
+      body = response.body;
+    })
+    it('A requisição GET para a rota retorna o status 200', () => {
+      expect(status).to.be.equal(200);
+    });
+    it('A requisição GET para a rota retorna um array de objetos', () => {
+      expect(body).to.be.an('object');
+    });
+    it('O objeto retornado possui as chaves "CodAtivo", "QtdeAtivo" e "Valor"', () => {
+      expect(body).to.include.all.keys("CodAtivo", "QtdeAtivo", "Valor");
+    });
+  })
+  describe('Quando é passado o CodAtivo de um ativo que não existe', () => {
+    let body: HTTPErrorMessage;
+    before(async () => {
+      const response = await chai.request(app).get('/ativos/200')
+      status = response.status;
+      body = response.body;
+    })
+    it('A requisição GET para a rota retorna o status 404', () => {
+      expect(status).to.be.equal(404);
+    });
+    it('A requisição GET para a rota retorna a mensagem "Esse ativo não existe"', () => {
+      expect(body.message).to.be.equal("Esse ativo não existe");
+    });
+  })
+
+  describe('Quando é passado um parâmetro que não é um número', () => {
+    let body: HTTPErrorMessage;
+    before(async () => {
+      const response = await chai.request(app).get('/ativos/teste')
+      status = response.status;
+      body = response.body;
+    })
+    it('A requisição GET para a rota retorna o status 404', () => {
+      expect(status).to.be.equal(404);
+    });
+    it('A requisição GET para a rota retorna a mensagem "Essa rota está incorreta"', () => {
+      expect(body.message).to.be.equal("Essa rota está incorreta");
+    });
+  })
 })
