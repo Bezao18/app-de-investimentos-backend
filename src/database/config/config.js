c3b7cb239e7d522147ae5569fefaf9eaf872ca61
Bernardo <bernardo.r.s18@gmail.com>
2022-07-24 15:38:56 -0300

    build(database config.js): dá a opção de utilizar variáveis de ambiente para o BD

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,11 +5,11 @@ const { DB_HOST, DB_PASSWORD, DATABASE, DB_USERNAME, DB_PORT } = process.env;
 
 module.exports = {
   "development": {
-    "username": 'root',
-    "password": 'password',
+    "username": DB_USERNAME || 'root',
+    "password": DB_PASSWORD || 'password',
     "database": 'app-de-investimentos',
-    "host": 'localhost',
-    "port": 3306,
+    "host": DB_HOST || 'localhost',
+    "port": DB_PORT || 3306,
     "dialect": "mysql"
   },
   "test": {
@@ -19,7 +19,7 @@ module.exports = {
     "host": 'localhost',
     "port": 3306,
     "dialect": "mysql"
-  
+
   },
   "production": {
     "username": 'root',
